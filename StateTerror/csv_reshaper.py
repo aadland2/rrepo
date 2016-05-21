0e1011f2ae4fe79322ff53e5c3fbc6144c09644e
Matt Aadland <aadland2@users.noreply.github.com>
2016-05-21 10:19:53 -0400

    right one this time

--- StateTerror/csv_reshaper.py
+++ StateTerror/csv_reshaper.py
@@ -7,7 +7,33 @@ Created on Fri May 20 19:19:38 2016
 import csv 
 path = "C:/Users/aadlandma/Desktop/migrationViz/static/csv"
 rights = path + "/predictedRights3.csv"
+out_path = path + "/out.csv"
+rows = []
 with open(rights, 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
-        print row
+        rows.append(row)
+
+
+ids = [row[9] for row in rows]
+years = [row[2] for row in rows]
+
+ids_i = list(set(ids))
+years_i = list(set(years)).sort()
+ids_i.sort()
+all_rows = []
+for i in ids_i:
+    print i
+    row_e = []
+    row_e.append(i)
+    for row in rows:
+        if row[9] == i:
+            row_e.extend([row[3]])
+    all_rows.append(row_e)
+
+
+
+b = open(out_path, 'w')
+a = csv.writer(b)
+a.writerows(all_rows)
+b.close()
